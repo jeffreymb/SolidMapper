b1d355e4a37df8053ce17a59e596b713206f45fb
StillLearnin <stilllearnin@thettpost.com>
2018-12-17 15:24:45 -0500

    Add mapper interface and extensions for dependency injection in aspnetcore.

---
+++ StableMapper/StableMapper/DITools.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace StableMapper
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static void AddStableMapper(this IServiceCollection services, Assembly assembly)
+        {
+            AddStableMapper(services, new Assembly[] { assembly });
+        }
+
+        public static void AddStableMapper(this IServiceCollection services, Assembly[] assemblies)
+        {
+            var mappers = assemblies.SelectMany(a => a.GetTypes().Where(t => t.GetInterfaces().Any(i => i.Namespace == "StableMapper"))).ToList();
+
+            foreach (var mapper in mappers)
+            {
+                services.AddScoped(Type.GetType(mapper.AssemblyQualifiedName));
+            }
+        }
+    }
+}
---
+++ StableMapper/StableMapper/IMapper.cs
@@ -0,0 +1,10 @@
+﻿namespace StableMapper
+{
+    public interface IMapper<TDTO, TEntity>
+    {
+        TEntity MapFrom(TDTO contactMethodEdit);
+        TDTO MapTo(TEntity method);
+        void Update(TEntity source, TDTO target);
+        void Update(TDTO source, TEntity target);
+    }
+}
