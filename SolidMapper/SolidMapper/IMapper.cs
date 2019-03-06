14e3517892e4cbbbb83d2f1d4abaf09fe640a911
StillLearnin <stilllearnin@thettpost.com>
2019-03-06 13:20:55 -0500

    clean up code / remove unused dependencies.

--- SolidMapper/SolidMapper/IMapper.cs
+++ SolidMapper/SolidMapper/IMapper.cs
@@ -3,8 +3,11 @@
     public interface IMapper<TDTO, TEntity>
     {
         TEntity Map(TDTO source);
+
         TDTO Map(TEntity source);
+
         void Map(TEntity source, TDTO target);
+
         void Map(TDTO source, TEntity target);
     }
 }
