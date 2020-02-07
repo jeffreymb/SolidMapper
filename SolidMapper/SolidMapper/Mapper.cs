cd7cc4cee6f83f1f6a5149e1532130eaa2d67dc1
Shawn Beachy <shawnthebeachy@outlook.com>
2020-02-07 08:50:24 -0500

    Add Mapper implementation.

---
+++ SolidMapper/SolidMapper/Mapper.cs
@@ -0,0 +1,27 @@
+﻿using SolidMapper.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SolidMapper
+{
+    public sealed class Mapper : IMapper
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public Mapper(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+        }
+
+        public Task<TDest> MapAsync<TSource, TDest>(TSource source, TDest dest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TDest>> MapRangeAsync<TSource, TDest>(IEnumerable<TSource> source, IEnumerable<TDest> dest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
